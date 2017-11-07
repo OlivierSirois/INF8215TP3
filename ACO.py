5e129de05872a746f8fe44b54f23fa6b17dee070
Olivier Sirois <olivier.sirois@polymtl.ca>
2017-11-07 13:02:12 -0500

    init

---
+++ ACO.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+from Graph import Graph
+from Solution import Solution
+
+SOURCE = 0
+
+
+class ACO(object):
+    def __init__(self, q0, beta, rho, phi, K, data):
+        self.parameter_q0 = q0
+        self.parameter_beta = beta
+        self.parameter_rho = rho
+        self.parameter_phi = phi
+        self.parameter_K = K
+
+        self.graph = Graph(data)
+        self.best = Solution(self.graph)
+        self.best.cost = 99999999999999
+        self.pheromone_init = np.ones((self.graph.N, self.graph.N))
+        f = open(data + '_init', 'r')
+        self.pheromone_init *= float(f.readline())
+        self.pheromone = np.ones((self.graph.N, self.graph.N))
+
+    def get_next_city(self, sol):
+        raise NotImplementedError()
+
+    def heuristic2opt(self, sol):
+        raise NotImplementedError()
+
+    def global_update(self, sol):
+        raise NotImplementedError()
+
+    def local_update(self, sol):
+        raise NotImplementedError()
+
+    def runACO(self, maxiteration):
+        raise NotImplementedError()
+
+# if __name__ == '__main__':
+#     aco = ACO(0.9, 2, 0.1, 0.1, 10, 'N12.data')
+#     aco.runACO(50)
