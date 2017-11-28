853a13ab35dbc4154a70fe60b71287117165ea57
Imanne El maachi <30446428+imanneelmaachi@users.noreply.github.com>
2017-11-28 12:31:47 -0500

    multiiprocessing tests

--- ACO_test_3.py
+++ ACO_test_3.py
@@ -91,43 +91,105 @@ def test_next_city():
     
 
 def runACO_test(q0,beta, rho, phi, K):
-    t1 = time()
+    
     aco = ACO(q0, beta, rho, phi, K, './tsp/qatar')
     aco.runACO(250)
-    t2 = time()
-    td = t2-t1
-    print('Time difference: , parameters ', td, q0,beta, rho, phi, K )
+    
+    
+
 if __name__ == '__main__':
     q0 = 0.9
     beta = 2
     rho = 0.1
     phi = 0.1
     K = 10
-    
+    #runACO_test(0.75,3, 0.16, 0.15, 10)
     #test_heuristic2opt()
     #test_global_update()
     #test_local_update()
     #test_next_city()
-    beta_list = np.array([1,2,3,4,5,6,7,8,9])
-    q0_list = np.array([0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.85,0.9,0.95])
-    processes = []
     
-    for q0 in q0_list:
-        processes = [mp.Process(target=runACO_test, args=(q0, beta, rho, phi, K)) for x in range(5)]
-        
+    beta_list = np.array([2.2,2.4,2.6,2.8,3])
+    q0_list = np.array([0.6,0.65,0.7,0.75,0.8,0.85,0.95]) #,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.85,0.9,0.95])
+    rho_list= np.array([0.12,0.14,0.15,0.16])
+    phi_list = np.array([0.1,0.11,0.13,0.15,0.17])
+    K_list = np.array([5,10,15,20,25])
+    
+    for n in range(0,5):  
+        processes = []
+        for beta in beta_list:
+            process = [mp.Process(target=runACO_test, args=(q0, beta, rho, phi, K))]
+            processes.extend(process)
         leng = len(processes)
         print('lenght', leng)
-        # Run processes
+            # Run processes
         for p in processes:
-            p.start()
-
-        # Exit the completed processes
+                p.start()
+    
+            # Exit the completed processes
         for p in processes:
-            p.join()
-
+                p.join()
         
-            
+        beta = 2    
+        processes = []
+        for q0 in q0_list:
+            process = [mp.Process(target=runACO_test, args=(q0, beta, rho, phi, K))]
+            processes.extend(process)
+        leng = len(processes)
+        print('lenght', leng)
+            # Run processes
+        for p in processes:
+                p.start()
+    
+            # Exit the completed processes
+        for p in processes:
+                p.join()        
+        q0 = 0.9
+        processes = []
+        for rho in rho_list:
+            process = [mp.Process(target=runACO_test, args=(q0, beta, rho, phi, K))]
+            processes.extend(process)
+        leng = len(processes)
+        print('lenght', leng)
+            # Run processes
+        for p in processes:
+                p.start()
+    
+            # Exit the completed processes
+        for p in processes:
+                p.join()           
         
+        rho = 0.1
+        processes = []
+        for phi in phi_list:
+            process = [mp.Process(target=runACO_test, args=(q0, beta, rho, phi, K)) for x in range(1)]
+            processes.extend(process)
+        leng = len(processes)
+        print('lenght', leng)
+            # Run processes
+        for p in processes:
+                p.start()
+    
+            # Exit the completed processes
+        for p in processes:
+                p.join()  
+                
+        phi = 0.1       
+        processes = []
+        for K in K_list:
+            process = [mp.Process(target=runACO_test, args=(q0, beta, rho, phi, K)) for x in range(1)]
+            processes.extend(process)
+        leng = len(processes)
+        print('lenght', leng)
+            # Run processes
+        for p in processes:
+                p.start()
+    
+            # Exit the completed processes
+        for p in processes:
+            p.join()             
+        K = 10
+    
     #print("prochaine iteration")
     #aco.runACO(1000)
     #print("prochaine iteration")
